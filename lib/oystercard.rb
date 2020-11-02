6d134d6ad738c43dbbfeeedc76f11a1b74e82fca
adam2021 <adam.patel101@outlook.com>
2020-11-02 17:34:15 +0000

    Can deduct money

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,26 @@
 class Oystercard
 
    attr_reader :balance
+   LIMIT= 90
 
   def initialize
-    balance = 0
+    @balance = 0
+  end
+
+  def top_up(amount)
+    @balance = @balance + amount
+    if @balance > LIMIT
+      @balance = @balance - amount
+      raise StandardError.new "Balance cannot exceed £#{LIMIT}"
+    else
+      return "Your current balance is £#{@balance}"
+    end
+  end
+
+  def deduct(fare)
+    @balance = @balance - fare
+    puts "You have been charged £#{fare}"
+    puts "Your current balance is £#{@balance}"
   end
 
 end
